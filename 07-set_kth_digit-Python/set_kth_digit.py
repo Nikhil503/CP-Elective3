1e1c07e9f130cddf6ca99d3a53b3345d79cab71e
Nikhil <nikhillinga670@msitprogram.net>
2021-07-14 12:06:36 +0530

    setkthdigit solution

--- 07-set_kth_digit-Python/set_kth_digit.py
+++ 07-set_kth_digit-Python/set_kth_digit.py
@@ -4,8 +4,24 @@
 # the kth digit replaced with d. Counting starts at 0 and goes right-to-left, 
 # so the 0th digit is the rightmost digit. 
 
-
-
 def fun_set_kth_digit(n, k, d):
-		return 0
+    flag = False
+    if(n < 0):
+        n = -n
+        flag = True
+    count = 0
+    result = 0
+    while(count<=k or n > 0):
+        r = n % 10
+        if count == k:
+            result = result +(d *(10 ** count))
+        else:
+            result = result + (r *(10 ** count))
+        count += 1
+        n = n // 10
+    
+    if flag:
+        result = -result
+   
+    return result
 
