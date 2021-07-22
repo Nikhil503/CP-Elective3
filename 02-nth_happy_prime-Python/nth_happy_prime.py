09b64f296c64b9c1eaafb413895280a502d4d64a
Nikhil <nikhillinga670@msitprogram.net>
2021-07-22 20:05:11 +0530

    nthhappyprime solution

--- 02-nth_happy_prime-Python/nth_happy_prime.py
+++ 02-nth_happy_prime-Python/nth_happy_prime.py
@@ -5,4 +5,39 @@
 
 
 def fun_nth_happy_prime(n):
-	return 0
+	a=0
+	b=0
+	while(a<=abs(n)):
+		b+=1
+		if(happyprime(b)):
+			a+=1
+	return b
+
+def prime(n):
+	if n<2:
+		return False
+	for i in range(2,n):
+		if n%i==0:
+			return False
+		return True 
+
+def happy(n):
+	s=0
+	while(n):
+		r=n%10
+		s+=r**2
+		n=n//10
+	if s==1:
+		return True
+	elif s<10:
+		return False
+	else:
+		return happy(s)
+
+def happyprime(n):
+	if prime(n)==True and happy(n)==True:
+		return True
+	else:
+		return False
+
+	
