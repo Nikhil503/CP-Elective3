3b4fa42b97b5dbc497708351a06bd9f43fe5b9aa
Nikhil <nikhillinga670@msitprogram.net>
2021-08-03 16:30:51 +0530

    happyprimes solution

--- 05-happyprimes-Python/happyprimes.py
+++ 05-happyprimes-Python/happyprimes.py
@@ -1,14 +1,38 @@
 # Happy Primes [20 pts]
 # Background: read the first paragraph from the Wikipedia page on happy numbers. After 
 # some thought, we see that no matter what number we start with, when we keep replacing 
-# the number by the sum of the squares of its digits, we'll always either arrive at 4 (
+# the number by the s of the squares of its digits, we'll always either arrive at 4 (
 # unhappy) or at 1 (happy). With that in mind, we want to write the function nthHappyNumber
 # (n). However, to write that function, we'll first need to write isHappyNumber(n) (
-# right?). And to write that function, we'll first need to write sumOfSquaresOfDigits(n). 
+# right?). And to write that function, we'll first need to write sOfSquaresOfDigits(n). 
 # And that's top-down design! Here we go.... 
 # Note: the autograder will grade each of the following functions, so they are required. 
 # However, they also are here specifically because they are just the right helper 
 # functions to make nthHappyNumber(n) easier to write!
+
+
+def prime(n):
+    if n<2:
+        return False
+    for i in range(2,n):
+        if(n%i == 0):
+            return False
+    return True
+
+def happynum(n):
+    s = 0
+    while(n!=0):
+        s+=(n%10)**2
+        n//=10
+    if s == 1:
+        return True
+    elif s<10:
+        return False
+    else:
+        return happynum(s)
+
 def ishappyprimenumber(n):
     # Your code goes here
-    pass
+    if(prime(n) and happynum(n)):
+        return True
+    return False
