94ddafe33bf98b96e2fb615053c13d43a7849622
Nikhil <nikhillinga670@msitprogram.net>
2021-08-03 16:42:05 +0530

    samechars solution

--- 07-samechars-Python/samechars.py
+++ 07-samechars-Python/samechars.py
@@ -6,5 +6,15 @@
 # parameter is not a string, but returns True if both strings are empty (why?).
 
 def samechars(s1, s2):
-	# Your code goes here
-	pass
+    	# Your code goes here
+	if type(s1) != type(s2):
+		return False
+	c = 0
+	for i in s2:
+		for j in range(len(s1)):
+			if(i == s1[j]):
+				c += 1
+				break
+	if (c == len(s2)):
+		return True
+	return False
